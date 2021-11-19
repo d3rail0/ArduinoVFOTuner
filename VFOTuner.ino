354f35278d0e725376726b6a9ba19f0bbc47b5f8
Eldin <eldinhelja@live.com>
2021-11-19 02:46:15 +0100

    Init

---
+++ VFOTuner.ino
@@ -0,0 +1,16 @@
+/*
+
+    Arduino VFO Tuner for Harris RF-550
+
+    Author: d3rail0
+    Tester: Keith Densmore
+
+    Completion date: Sunday the 5th of November, 2017 
+
+*/
+
+#define encoder0PinA 3
+#define encoder0PinB 2
+#define SPEED_BUTON 12
+#define TUNE_SWITCH 17
+
